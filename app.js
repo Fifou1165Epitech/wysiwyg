aa6ce6094f2de2aa42220bb29b8327f8c6475a64
root <mathis1.nnomo@epitech.eu>
2025-02-01 20:10:19 +0100

    [update] CSS Update

---
+++ app.js
@@ -0,0 +1,74 @@
+import { MyWysiwyg } from "./modules/my_wysiwyg.js";
+
+let textArea = document.getElementById("textarea");
+
+let mw = new MyWysiwyg(document.getElementById("textarea"),{
+    buttons : ["Gras", "Italic", "TextCrossed", "Choix de la couleur", "Taille de la police", "Lien"]
+});
+let i = 0;
+textArea.addEventListener("keydown", (e) => {
+    if(e.key == "Enter" && e.shiftKey){
+        return;
+    }else if(e.key == "Enter"){
+        e.preventDefault();
+        let div = document.createElement('div');
+        let t = mw.createToolbar(div);
+
+        div.style.display = "flex";
+        div.style.alignItems = "center";
+        div.id = `${i}`;
+        i++;
+        let gras = t.querySelector(".Gras");
+        let italic = t.querySelector(".Italic");
+        let textCrossed = t.querySelector(".TextCrossed");
+
+
+
+        let p = document.createElement('p');
+        p.style.width = "100%";
+        p.contentEditable = 'true';
+        p.style.backgroundColor = 'white';
+        div.appendChild(p);
+        textArea.appendChild(div);
+        p.focus();
+
+
+        let boldIndex = 0;
+        gras.addEventListener("click", () => {
+            p.style.fontWeight = "bold";
+            if(boldIndex == 0){
+                p.style.fontWeight = "bold";
+                boldIndex++;
+            }else if(boldIndex == 1){
+                p.style.fontWeight = "normal";
+                boldIndex--;
+            }
+        })
+
+        let italicIndex = 0;
+        italic.addEventListener("click", () => {
+            p.style.fontStyle = "italic";
+            if(italicIndex == 0){
+                p.style.fontStyle = "italic";
+                italicIndex++;
+            }else if(italicIndex == 1){
+                p.style.fontStyle = "normal";
+                italicIndex--;
+            }
+        })
+
+        let textCrossedIndex = 0;
+        textCrossed.addEventListener("click", () => {
+            p.style.textDecorationLine = "line-through";
+            if(textCrossedIndex == 0){
+                p.style.textDecorationLine = "line-through";
+                textCrossedIndex++;
+            }else if(textCrossedIndex == 1){
+                p.style.textDecorationLine = "none";
+                textCrossedIndex--;
+            }
+        })
+
+    }
+    
+})
